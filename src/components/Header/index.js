f90df3c1bf54c499a93f42cb4b76613861d507c4
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-12-30 17:58:07 -0600

    started header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+        <Link to="/"> Jake Wilson </Link>
+      <nav>
+        
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
