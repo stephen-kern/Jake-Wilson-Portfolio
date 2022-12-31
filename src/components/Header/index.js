65a6167b8d5bb06f128b2b50da05419021315e63
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-12-31 15:00:04 -0600

    base header set up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,12 +4,30 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-        <Link to="/"> Jake Wilson </Link>
-      <nav>
-        
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/" className="logo">
+            Jake Wilson
+          </Link>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/collagraphs">Collagraphs</Link>
+          </li>
+          <li>
+            <Link to="/etchings">Etchings</Link>
+          </li>
+          <li>
+            <Link to="/relief">Relief</Link>
+          </li>
+        </ul>
+        <div className="right-links">
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
