71485537f6d301b78f227e73556eb12b0d7698e3
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-01-19 13:54:54 -0600

    commiting for break

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react";
 
 import JakeStudio from "../../assets/images/jakestudio.jpg";
 
-
-
 const Home = () => {
-    return(
-        <div className='bio-container'>
-            <div className='bio-image-container'>
-                <img src={JakeStudio} alt='#'></img>
-            </div>
-            <div className='bio'>
-                <h2>Jake Wilson</h2>
-                <p>bio for jake</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bio-container">
+      <img src={JakeStudio} alt="#"></img>
+      <div className="bio">
+        <h2>Jake Wilson</h2>
+        <p>bio for jake</p>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
