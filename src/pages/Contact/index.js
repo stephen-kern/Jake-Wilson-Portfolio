ca2efe250dcb8e04123b82bf9896732ab58ca224
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-01-18 18:51:57 -0600

    desktop contact form added and styled

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import JakeStudio from "../../assets/images/JakePress.jpg";
 
 const ContactUs = () => {
   const form = useRef();
@@ -25,16 +26,22 @@ const ContactUs = () => {
   };
 
   return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+    <div className="form-container">
+      <div className="form-img-container">
+        <img src={JakeStudio} alt="Jake Wilson using the printing press"></img>
+      </div>
+      <div className="email-form">
+        <h4 className="contact-title"> Contact Jake Wilson </h4>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="SEND" className="contact-btn"/>
+        </form>
+      </div>
     </div>
   );
 };
