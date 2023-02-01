e55cb428d06b34cb191ead98d6fdde0d58ccd0a8
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-02-01 14:15:19 -0600

    adjusted email form. tested in browser with responsiveness

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,15 +1,33 @@
-import { React, useRef,  } from "react";
+import { React, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+
+import CheckIcon from "@mui/icons-material/Check";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+
 import JakeStudio from "../../assets/images/JakePress.jpg";
 
 const ContactUs = () => {
+  const [emailSent, setEmailSent] = useState(false);
+  const [emailError, setEmailError] = useState(false);
   const form = useRef();
   const resetForm = () => {
     form.current.reset();
   };
 
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const validateEmail = (email) => {
+    if (!emailRegex.test(email)) {
+      setEmailError(true);
+      return false;
+    }
+    setEmailError(false);
+    return true;
+  };
+
   const sendEmail = (e) => {
     e.preventDefault();
+    const email = form.current.elements.user_email.value;
+    if (!validateEmail(email)) return;
     emailjs
       .sendForm(
         process.env.REACT_APP_SERVICE_ID,
@@ -21,6 +39,10 @@ const ContactUs = () => {
         (result) => {
           console.log(result.text);
           resetForm();
+          setEmailSent(true);
+          setTimeout(() => {
+            setEmailSent(false);
+          }, 5000);
         },
         (error) => {
           console.log(error.text);
@@ -41,14 +63,25 @@ const ContactUs = () => {
             sendEmail(e);
           }}
         >
-          <label>Name</label>
-          <input type="text" name="user_name" required/>
-          <label>Email</label>
-          <input type="email" name="user_email" required/>
-          <label>Message</label>
-          <textarea name="message" required/>
+          <label>Name *</label>
+          <input type="text" name="user_name" required />
+          <label>Email *</label>
+          <input type="email" name="user_email" required />
+          <label>Message *</label>
+          <textarea name="message" required />
           <input type="submit" value="SEND" className="contact-btn" />
         </form>
+        {emailError && (
+          <div className="email-message fade">
+            <ErrorOutlineIcon className="email-error" /> Please enter a valid
+            email address.
+          </div>
+        )}
+        {emailSent && (
+          <div className="email-message fade">
+            <CheckIcon className="email-checkmark" /> Email sent!
+          </div>
+        )}
       </div>
     </div>
   );
